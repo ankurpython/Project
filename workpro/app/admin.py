4ad049b2b241b097c91869a3c07b6b12d6fb5342
suman shekhar <shekhar.shs@gmail.com>
2019-10-07 14:51:36 +0530

    Work PRo

---
+++ workpro/app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from app.models import FibonacciTest
+
+admin.site.register(FibonacciTest)
---
+++ workpro/app/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+from django.db import models
+
+class FibonacciTest(models.Model):
+	latency = models.CharField(max_length=100, blank=True, null=True)
+	numeric = models.PositiveIntegerField(blank=True, null=True)
+	output = models.PositiveIntegerField(blank=True, null=True)
+	
+	def __str__(self):
+		return self.output
+
+	
+        
